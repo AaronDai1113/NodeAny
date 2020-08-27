fc1c6f96bae02262e31b96fd9d716f6941e3c354
AaronDai1113 <aaron11388@outlook.com>
2020-08-27 22:50:07 +0800

    fac

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Customer{
+    public static Food Cook(string type){
+        Food food=null;
+        if(type.Equals("A")){
+            
+        }
+        return food;
+    }
+}
+
+public abstract class Food{
+    public abstract void Print();
+}
