3199d07720160624ee3a298d545b4591e4f18093
AaronDai1113 <aaron11388@outlook.com>
2020-09-21 16:26:39 +0800

    delegate

--- Program.cs
+++ Program.cs
@@ -2,16 +2,38 @@
 
 namespace DotNetCoreSample
 {
-    class Program
+    public delegate int Transform(int x);
+    public delegate void ProgressReporter(int percentComplete);
+    public delegate T Transformer<T>(T arg);
+    class X{
+        public void InstanceProgress(int percentComplete)=>Console.WriteLine(percentComplete);
+    }
+    class Util{
+        public static void Transform<T>(T[] values,Func<T,T> t){
+            for(int i=0;i<values.Length;i++){
+                values[i]=t(values[i]);
+            }
+        }
+    }
+    public class Program
     {
+        static int Square(int x){
+            var result=x*x;
+            
+            return result;
+        }
+
+        static int Cube(int x){
+            var result=x*x*x;
+            Console.WriteLine(result);
+            return result;
+        }
         static void Main(string[] args)
         {
-            Food food1=FoodSimpleFactory.CreateFood("A");
-            food1.Print();
-
-            Food food2=FoodSimpleFactory.CreateFood("B");
-            food2.Print();
-            Console.Read();
+            int[] values={1,2,3};
+            Util.Transform(values,Square);
+            foreach(int i in values)
+                Console.Write(i+" ");
         }
     }
 }
